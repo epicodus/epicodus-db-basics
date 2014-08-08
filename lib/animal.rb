34fe64b73b27bb2824c4233a13d5215eea61bb38
jeff.j.stringer@gmail.com <jeff.j.stringer@gmail.com>
2014-08-07 21:40:14 -0700

    add .all method to Animal class

--- lib/animal.rb
+++ lib/animal.rb
@@ -11,4 +11,11 @@ class Animal
     result = DB.exec("INSERT INTO animals (species) VALUES ('#{@species}') RETURNING id;")
     @id = result.first['id']
   end
+
+  def self.all
+    animals = []
+    results = DB.exec("SELECT * FROM animals;")
+    results.each { |result| animals << Animal.new(result) }
+    animals  
+  end
 end
--- spec/animal_spec.rb
+++ spec/animal_spec.rb
@@ -11,5 +11,15 @@ describe Animal do
     animal = Animal.new({'species' => 'lion'})
     animal.save
     expect(Animal.all).to eq [animal]
+  end
+
+  describe '.all' do
+    it 'creates animal objects from all animal entries in db' do
+      animal = Animal.new({'species' => 'lion'})
+      another_animal = animal = Animal.new({'species' => 'tiger'})
+      animal.save
+      another_animal.save
+      expect(Animal.all).to eq [animal, another_animal]
+    end  
   end  
 end
