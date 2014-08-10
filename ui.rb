761936920c21d02034ac9cdcc28813bc6fb167ad
jeff.j.stringer@gmail.com <jeff.j.stringer@gmail.com>
2014-08-10 14:21:08 -0700

    add list_all_animals_by_trainer to ui.rb

--- ui.rb
+++ ui.rb
@@ -17,7 +17,7 @@ def main_menu
     puts "Select 5 to list all trainers"
     puts "Select 6 to list all all species trained by a trainer"
     puts "Select 7 to list all all animals trained by a trainer"
-    puts "Select 8 to list all animals of a species for a trainer"
+    puts "Select 8 to list all animals of given species for a trainer"
     puts "Select * to exit"
     puts "\n"
     print ">"
@@ -29,6 +29,7 @@ def main_menu
       when '4' then add_animal_to_trainer
       when '5' then list_all_trainers
       when '6' then list_all_species_by_trainer
+      when '7' then list_all_animals_by_trainer
       when '*' then exit
     end
   end
@@ -86,6 +87,16 @@ def list_all_species_by_trainer
   trainer = Trainer.find_by_name(trainer_name)
   puts "All species trained by #{trainer.name}:\n"
   puts trainer.list_kinds
+  puts "\n"
+end
+
+def list_all_animals_by_trainer
+  list_all_trainers
+  puts "Select a trainer to view all species:\n"
+  print ">"; trainer_name = gets.chomp
+  trainer = Trainer.find_by_name(trainer_name)
+  puts "All animals trained by #{trainer.name}:\n"
+  puts trainer.list_animals
   puts "\n"
 end
 
